b61984411c8d2bebe57e270de36e4724b504ecaf
Mohamad-Jaallouk <xzxz22@gmail.com>
2023-03-20 14:12:13 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Squat Counter",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
