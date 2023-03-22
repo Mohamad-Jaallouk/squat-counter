dfb903976f99519709460b77539fe6bb29dbe6c7
Mohamad-Jaallouk <xzxz22@gmail.com>
2023-03-22 21:07:48 +0100

    Fix Camera

--- app/useWebcam.ts
+++ app/useWebcam.ts
@@ -2,15 +2,12 @@ import { useRef, useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-cpu";
 import { WebcamIterator } from "@tensorflow/tfjs-data/dist/iterators/webcam_iterator";
-import useCameraPermission from "./useCameraPermission";
 
 const useWebcam = (webcamEnabled: boolean) => {
-  console.log("Run useWebcam");
   const webcamRef = useRef<HTMLVideoElement | null>(null);
   const [webcam, setWebcam] = useState<WebcamIterator | undefined>();
 
   useEffect(() => {
-    console.log("Run useWebcam useEffect");
     const initializeWebcam = async () => {
       if (!webcamRef.current) return;
 
@@ -19,10 +16,6 @@ const useWebcam = (webcamEnabled: boolean) => {
       try {
         webcamRef.current.width = 1280;
         webcamRef.current.height = 720;
-        const videoElement = document.createElement("video");
-        // videoElement.width = 100;
-        // videoElement.height = 100;
-        // const initializedWebcam = await tf.data.webcam(videoElement);
         const initializedWebcam = await tf.data.webcam(webcamRef.current);
         setWebcam(initializedWebcam);
       } catch (error) {
