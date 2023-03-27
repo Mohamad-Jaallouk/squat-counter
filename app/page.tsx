faac0dce6c196a0b75196c74191e454fd51afea9
Mohamad-Jaallouk <xzxz22@gmail.com>
2023-03-27 17:48:44 +0200

    WORKING 4

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Squat from "./squat";
 export default function Home() {
   return (
     <>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
       <Squat nReps={20} />
     </>
   );
